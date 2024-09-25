95507edac057dcdc7adffe1b0810ef794d7d5a96
Nicola Clark <nicola@slottedspoon.dev>
2024-09-24 23:29:25 -0500

    we have an HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-  println!("howdy y'all!!!");
+use std::process;
+
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+  pretty_env_logger::init_custom_env("BACKEND_LOG_LEVEL");
+  log::info!("starting ao3pub-back v{}", env!("CARGO_PKG_VERSION"));
+
+  let app = Router::new().route("/", get(root));
+
+  let listener = match TcpListener::bind("0.0.0.0:8080").await {
+    Ok(listener) => listener,
+    Err(error) => {
+      log::error!("couldn't bind to port: {error}");
+      process::exit(1);
+    }
+  };
+
+  log::info!("ao3pub-back started");
+
+  if let Err(error) = axum::serve(listener, app).await {
+    log::error!("error serving app: {error}");
+    process::exit(1);
+  }
+}
+
+async fn root() -> &'static str {
+  "howdy y'all!!!"
 }
